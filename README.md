06904243619c5f671a95a33be6d7e234d4d7fa67
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2026-04-24 22:48:53 -0300

    Interface adicionada

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # tad-vetor
+O vetor é um tipo abstrato de dados que determina que a manipulação dos dados ocorra através de índices numéricos, assim como no array.
+
+Por isso, talvez seja da implementação mais natural usar um array para armazenar os dados do vetor.
---
+++ src/interfaces/VetorInterface.java
@@ -0,0 +1,16 @@
+package interfaces;
+
+public interface VetorInterface {
+    /**
+    * A interface a seguir deve funcionar tanto para implementações que usam arrays
+     * quanto para as que usam listas.
+     *
+     * Foi traduzida para o Português apenas por capricho didático.
+     */
+    public Object elementoNaColocacao(int colocacao);
+    public Object substituirElementoNaColocacao(int colocacao, Object novoElemento);
+    public void inserirElementoNaColocacao(int colocacao, Object novoElemento);
+    public Object removerElementoNaColocacao(int colocacao);
+    public int tamanho();
+    public boolean estaVazia();
+}
